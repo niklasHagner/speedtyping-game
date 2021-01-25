a4efc4b00a6245f28034c6a3b3aed9f82ea4c93a
niklasHagner <niklas.hagner@outlook.com>
2021-01-25 22:31:32 +0100

    Add playerManager helper

---
+++ playerManager.js
@@ -0,0 +1,55 @@
+module.exports = {
+  setUpNewPlayer,
+  getPlayerByName,
+  leave,
+  updateScore
+};
+
+const players = [];
+
+const avatars = [
+  {emoji:"🐸", occupied: false},
+  {emoji:"🐷", occupied: false},
+  {emoji:"🐹", occupied: false},
+  {emoji:"🐵", occupied: false},
+  {emoji:"🐱", occupied: false},
+  {emoji:"🐤", occupied: false},
+];
+
+function getAvatar() {
+  const availableAvatars = avatars.filter(x => !x.occupied);
+  if (availableAvatars.length === 0) { //reset availability
+      avatars.forEach(x => x.occupied = false);
+  }
+  const ix = Math.floor(Math.random() * availableAvatars.length);
+  avatars[ix].occupied = true;
+  return avatars[ix].emoji;
+}
+
+function setUpNewPlayer(username) {
+  const avatar = getAvatar();
+  const score = 0;
+
+  const player = { username, avatar, score };
+  console.log(player);
+  players.push(player);
+  return player;
+}
+
+function getPlayerByName(username) {
+  return players.find(x => x.username === username);
+}
+
+function getPlayerIndexByUserName(username) {
+  return players.indexOf(getPlayerByName(username));
+}
+
+function leave(username) {
+  return players.splice(getPlayerIndexByUserName(username), 1);
+}
+
+function updateScore(username, score) {
+  const ix = getPlayerIndexByUserName(username);
+  players[ix].score += score;
+  return players[ix];
+}
