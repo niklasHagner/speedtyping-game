554fcac2e98786b72d94052d90343c9b47ae9ccb
niklasHagner <niklas.hagner@outlook.com>
2023-08-13 13:57:32 +0200

    Use gameStates and resets

--- public/speedtyping/speedtyping.js
+++ public/speedtyping/speedtyping.js
@@ -15,35 +15,35 @@ const newGameButtonContainer = document.getElementById("new-game-button-containe
 const url = window.location.href;
 const socket = io.connect(url);
 const GAME = {
-    totalTargetText: "",
-    nextWordTarget: "",
-    remainingTargetText: "",
-    remainingTargetTextSplit: [],
-    incorrectInput: "",
-    correctInCurrentWord: "",
-    correctOldWords: "",
-    username: null,
-    states: [
-      { name: "unstarted", active: true },
-      { name: "started", active: false },
-      { name: "match-completed", active: false },
-    ]
+  totalTargetText: "",
+  nextWordTarget: "",
+  remainingTargetText: "",
+  remainingTargetTextSplit: [],
+  incorrectInput: "",
+  correctInCurrentWord: "",
+  correctOldWords: "",
+  username: null,
+  states: [
+    { name: "unstarted", active: true },
+    { name: "started", active: false },
+    { name: "match-completed", active: false },
+  ]
 }
 
 //---Socket listeners---
 let lastMessageTime;
 socket.on("server_message", function (msg) {
-    serverMessage.innerHTML = msg;
-    serverMessage.classList.remove("hidden");
-    lastMessageTime =  new Date();
-    window.setTimeout(() => {
-        const nowTime = new Date().getTime();
-        const diff = lastMessageTime && nowTime - lastMessageTime.getTime();
-        console.log("diff", diff);
-        if (diff > 1100) {
-            serverMessage.classList.add("hidden");
-        }
-    }, 1500);
+  serverMessage.innerHTML = msg;
+  serverMessage.classList.remove("hidden");
+  lastMessageTime = new Date();
+  window.setTimeout(() => {
+    const nowTime = new Date().getTime();
+    const diff = lastMessageTime && nowTime - lastMessageTime.getTime();
+    console.log("diff", diff);
+    if (diff > 1100) {
+      serverMessage.classList.add("hidden");
+    }
+  }, 1500);
 });
 
 socket.on("target_sentence", resetStuffAsNewSenteceAppears);
@@ -53,11 +53,11 @@ socket.on("show_players", function (data) {
   const players = data.players;
   players.forEach(x => x.isYou = x.username === GAME.username);
 
-  const playersHtml = 
+  const playersHtml =
     players
-    .sort((a,b) => a.username === b.username ? 0 : (a.username === GAME.username ? -1 : 1) )
-    .map(x => `
-      <div class="player-row ${ x.isYou ? 'player-row--you' : '' }">
+      .sort((a, b) => a.username === b.username ? 0 : (a.username === GAME.username ? -1 : 1))
+      .map(x => `
+      <div class="player-row ${x.isYou ? 'player-row--you' : ''}">
           <div class="player-row__inner">
             <span>${x.username} ${x.isYou ? ' (you)' : ''}</span>
             <span class="score">${x.wordsPerMinute} wpm</span>
@@ -71,7 +71,7 @@ socket.on("show_players", function (data) {
 });
 
 socket.on("player_finished", function (playerData) {
-  GAME.states.find(state => state.name === "match-completed").active = true;
+  setGameState("match-completed");
   document.body.classList.add("game-state--match-completed");
   chatForm.classList.add("hidden");
   let newEl = document.createElement("div");
@@ -81,16 +81,17 @@ socket.on("player_finished", function (playerData) {
       <p>Words per minute:${playerData.wordsPerMinute}</p>
       <p>Chars written:${playerData.charsSoFar}</p>
       <p>Chars per minute:${playerData.charsPerMinute}</p>
-      <button onclick="clickStartNewGame()">New game</button>
+      <button>New game</button>
   `;
   let footer = document.querySelector("footer");
   footer.parentNode.insertBefore(newEl, footer);
+  document.querySelector("#match-completed-screen button").addEventListener("click", clickStartNewGame);
 });
 
 function askUserName() {
-    const usernameModal = document.createElement("div");
-    usernameModal.id = "username-modal";
-    usernameModal.innerHTML = `
+  const usernameModal = document.createElement("div");
+  usernameModal.id = "username-modal";
+  usernameModal.innerHTML = `
         <h2>Enter your name</h2>
         <form id="username-form">
             <input style="width: 10ch;" type="text" minlength="1" maxlength="10" tabindex="-1">
@@ -99,120 +100,133 @@ function askUserName() {
         <p class="hidden">Names have to be 1 to 10 chars</p>
     `;
 
-    document.querySelector("main").append(usernameModal);
-    const usernameForm = document.querySelector("#username-form");
-    const usernameInput = usernameForm.querySelector("input");
-    window.setTimeout(function () {
-        usernameInput.focus();
-    }, 1);
+  document.querySelector("main").append(usernameModal);
+  const usernameForm = document.querySelector("#username-form");
+  const usernameInput = usernameForm.querySelector("input");
+  window.setTimeout(function () {
+    usernameInput.focus();
+  }, 1);
 
-    //Speed up development with a preset username
-    usernameInput.value = getRandomName();
-    
-    usernameForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const username = usernameInput.value;
-        if (username && username.length > 0 && username.length < 10) {
-            acceptUserNameAndStart(username);
-        } else {
-            usernameModal.querySelector("p").classList.remove("hidden");
-        }
-    });
+  //Speed up development with a preset username
+  usernameInput.value = getRandomName();
 
-    function acceptUserNameAndStart(username) {
-        GAME.username = username;
-        socket.emit("new_player_with_username_joined", username);
-        // document.getElementById("chat-form").classList.remove("hidden");
-        // targetSentenceContainer.classList.remove("hidden");
-        usernameModal.classList.add("hidden");
-        window.setTimeout(function () {
-            chatFormInput.focus();
-        }, 1);
+  usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = usernameInput.value;
+    if (username && username.length > 0 && username.length < 10) {
+      acceptUserNameAndStart(username);
+    } else {
+      usernameModal.querySelector("p").classList.remove("hidden");
     }
+  });
+
+  function acceptUserNameAndStart(username) {
+    GAME.username = username;
+    socket.emit("new_player_with_username_joined", username);
+    // document.getElementById("chat-form").classList.remove("hidden");
+    // targetSentenceContainer.classList.remove("hidden");
+    usernameModal.classList.add("hidden");
+    window.setTimeout(function () {
+      chatFormInput.focus();
+    }, 1);
+  }
 }
 
 //--- DOM event listeners ---
 chatFormInput.addEventListener('input', () => {
   const input = chatFormInput.value;
-  
-  if (GAME.nextWordTarget.indexOf(input) !==0 ) {
-      GAME.incorrectInput = input;
+
+  if (GAME.nextWordTarget.indexOf(input) !== 0) {
+    GAME.incorrectInput = input;
   } else {
-      GAME.incorrectInput = "";
+    GAME.incorrectInput = "";
   }
 
   if (GAME.nextWordTarget.indexOf(input) === 0) {
-      GAME.correctInCurrentWord = input;
-      GAME.totalCorrect = GAME.correctOldWords + GAME.correctInCurrentWord;
-      sentenceInProgressEl.innerText = GAME.totalCorrect;
-      // console.log("correct so far:", GAME.correctInCurrentWord, "remaining:", GAME.remainingTargetText, GAME.remainingTargetTextSplit);
-      GAME.remainingTargetText = GAME.totalTargetText.substr(GAME.totalCorrect.length, GAME.totalTargetText.length)
-      sentenceRemainingEl.innerText = GAME.remainingTargetText;
+    GAME.correctInCurrentWord = input;
+    GAME.totalCorrect = GAME.correctOldWords + GAME.correctInCurrentWord;
+    sentenceInProgressEl.innerText = GAME.totalCorrect;
+    // console.log("correct so far:", GAME.correctInCurrentWord, "remaining:", GAME.remainingTargetText, GAME.remainingTargetTextSplit);
+    GAME.remainingTargetText = GAME.totalTargetText.substr(GAME.totalCorrect.length, GAME.totalTargetText.length)
+    sentenceRemainingEl.innerText = GAME.remainingTargetText;
   }
   if (GAME.nextWordTarget === input) {
-      socket.emit("player_move", input);
-      chatFormInput.value = "";
-      GAME.correctOldWords += GAME.totalTargetTextSplit[0];
-      GAME.totalTargetTextSplit.shift();
-      GAME.nextWordTarget = GAME.totalTargetTextSplit[0];
-  } 
+    socket.emit("player_move", input);
+    chatFormInput.value = "";
+    GAME.correctOldWords += GAME.totalTargetTextSplit[0];
+    GAME.totalTargetTextSplit.shift();
+    GAME.nextWordTarget = GAME.totalTargetTextSplit[0];
+  }
 
   if (GAME.incorrectInput.length > 0) {
-      chatForm.classList.add("error");
+    chatForm.classList.add("error");
   } else {
-      chatForm.classList.remove("error");
+    chatForm.classList.remove("error");
   }
 });
 
 //--- Helpers ---
 function clickStartNewGame(e) {
-    socket.emit("start_new_game", null);
-    console.log("start_new_game");
-    targetSentenceContainer.classList.remove("hidden");
-    chatForm.classList.remove("hidden");
+  socket.emit("start_new_game", null);
+  console.log("start_new_game");
+  targetSentenceContainer.classList.remove("hidden");
+  chatForm.classList.remove("hidden");
 
-    resetStuffBeforeNewGame();
-    if (e.target) {
-      e.target.remove();
-    }
+  resetStuffBeforeNewGame();
+  if (e.target) {
+    e.target.remove();
+  }
 }
 
 function resetStuffBeforeNewGame() {
-    chatForm.classList.remove("hidden");
-    const matchCompletedScreen = document.querySelector("#match-completed-screen");
-    if (matchCompletedScreen) matchCompletedScreen.remove();
+  setGameState("unstarted");
+  document.body.classList.remove("game-state--match-completed");
 
-    const firstPlayerNewGameEl = document.querySelector("#new-game-button-container");
-    if (firstPlayerNewGameEl) firstPlayerNewGameEl.remove();
+  chatForm.classList.remove("hidden");
+  const matchCompletedScreen = document.querySelector("#match-completed-screen");
+  if (matchCompletedScreen) matchCompletedScreen.remove();
+
+  const firstPlayerNewGameEl = document.querySelector("#new-game-button-container");
+  if (firstPlayerNewGameEl) firstPlayerNewGameEl.remove();
+}
+
+function setGameState(stateName) {
+  GAME.states.forEach(state => state.active = false);
+
+  const found = GAME.states.find(state => state.name === stateName);
+  if (found) found.active = true;
 }
 
 function resetStuffAsNewSenteceAppears(msg) {
-    GAME.incorrectInput = ""
-    GAME.correctInCurrentWord = ""
-    GAME.correctOldWords = ""
-    GAME.totalTargetText = msg;
-    GAME.remainingTargetText = GAME.totalTargetText;
-    const split = GAME.remainingTargetText.split(" ");
-    let i = split.length -1;
-    do {
-        split.splice(i, 0, " ");
-    } while (i-- && i > 0)
+  setGameState("started");
+  GAME.incorrectInput = ""
+  GAME.correctInCurrentWord = ""
+  GAME.correctOldWords = ""
+  GAME.totalTargetText = msg;
+  GAME.remainingTargetText = GAME.totalTargetText;
+  const split = GAME.remainingTargetText.split(" ");
+  let i = split.length - 1;
+  do {
+    split.splice(i, 0, " ");
+  } while (i-- && i > 0)
 
-    GAME.totalTargetTextSplit = split;
-    GAME.nextWordTarget = split[0];
+  GAME.totalTargetTextSplit = split;
+  GAME.nextWordTarget = split[0];
 
-    sentenceRemainingEl.innerText = msg;
-    sentenceInProgressEl.innerText = "";
-    chatFormInput.focus();
-    const matchCompletedScreen = document.querySelector("#match-completed-screen");
-    if (matchCompletedScreen) matchCompletedScreen.remove();
+  sentenceRemainingEl.innerText = msg;
+  sentenceInProgressEl.innerText = "";
+  chatFormInput.focus();
+  const matchCompletedScreen = document.querySelector("#match-completed-screen");
+  if (matchCompletedScreen) matchCompletedScreen.remove();
+
+  document.body.classList.remove("game-state--match-completed");
 }
 
 function giveFirstPlayerStartButton() {
-    const btn = document.createElement('button')
-    btn.innerText = "New game";
-    btn.addEventListener("click", clickStartNewGame);
-    newGameButtonContainer.appendChild(btn);
+  const btn = document.createElement('button')
+  btn.innerText = "New game";
+  btn.addEventListener("click", clickStartNewGame);
+  newGameButtonContainer.appendChild(btn);
 }
 
 //Initial load
